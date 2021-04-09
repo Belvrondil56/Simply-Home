5c55b3c8f1d99ff96f91dde2596f5c7807ace922
GuillaumeM <gu.martin@yahoo.com>
2021-04-09 12:34:39 +0200

    Add sene pages, modify style house css to center the image vertically

---
+++ sene1.php
@@ -0,0 +1,48 @@
+<?php include("header.php") ?>
+
+<main class="houseSingle largeurMax">
+    
+        <h2 class="title">Maison 6 pièces 120 m²</h2>
+
+        <div class="house">
+
+            <div class="houseImg">                
+                <img src="images/images/sene1.jpg" alt="Maison example">
+            </div>
+
+            <div class="houseInfoBlock">
+                <div class="infos">
+                    <div class="city" title="Ville"><strong><img src="images/images/localisation.png" alt="Localisation">Ville</strong> : Séné 56860</div>
+                    <div class="surface" title="Surface"><strong><img src="images/images/meters.png" alt="Surface">Surface</strong> : 120 m²</div>
+                    <div class="price" title="Prix"><strong><img src="images/images/price.png" alt="Prix">Prix</strong> : 357 000 €</div>
+                    <div class="energetic" title="Classe énergie"><strong><img src="images/images/energy.png" alt="Classe énergie">Classe énergie</strong> : D</div>
+                    <div class="year" title="Année"><strong><img src="images/images/calendar.png" alt="Année">Année</strong> : 2015</div>
+                </div>
+
+                <p class="houseDescription">
+                    <h3 class="descTitle">Description</h3>
+                    Rejoindre le bourg par les sentiers côtiers...et Vannes à vélo...!
+
+                    Cette maison offre un emplacement stratégique et bucolique.
+
+                    A rafraîchir au RDC, un espace de vie avec cuisine ouverte et accès direct sur le jardin de 600 m2, une chambre, salle de bain, une buanderie, entrée, WC
+
+                    A l'étage, 3 chambres et une salle d'eau.
+
+                    Un abri de jardin
+
+                    Maison familiale, RARE dans cette fourchette de prix!
+
+                    Les honoraires d'agence sont à la charge de l'acquéreur, soit 5,00% TTC du prix du bien. Contactez  Annabelle CAMUT, Agent commercial OptimHome (RSAC N°527618508 Greffe de VANNES) 06 89 14 44 30 *** (réf. 493541 )
+                    Référence annonce : 830038230772
+                    Prix hors honoraires : 340 000 €
+                </p>
+
+                <div class="fees" title="Honoraire"><strong>Honoraires</strong> : Le prix comprend les honoraires à la charge de l'acheteur</div>
+            </div>
+
+    </div>
+
+</main>
+
+<?php include("footer.php") ?>
---
+++ sene4.php
@@ -0,0 +1,40 @@
+<?php include("header.php") ?>
+
+<main class="houseSingle largeurMax">
+
+    <h2 class="title">3 km du Port de Vannes maison 4 chambres 128 m² terrain 495 m²</h2>
+
+    <div class="house">
+
+        <div class="houseImg">
+            <img src="images/images/sene4.jpg" alt="Maison example">
+        </div>
+
+        <div class="houseInfoBlock">
+            <div class="infos">
+                <div class="city" title="Ville"><strong><img src="images/images/localisation.png" alt="Localisation">Ville</strong> : Séné 56860</div>
+                <div class="surface" title="Surface"><strong><img src="images/images/meters.png" alt="Surface">Surface</strong> : 119 m²</div>
+                <div class="price" title="Prix"><strong><img src="images/images/price.png" alt="Prix">Prix</strong> : 487 900 €</div>
+                <div class="energetic" title="Classe énergie"><strong><img src="images/images/energy.png" alt="Classe énergie">Classe énergie</strong> : C</div>
+                <div class="year" title="Année"><strong><img src="images/images/calendar.png" alt="Année">Année</strong> : 2016</div>
+            </div>
+
+            <p class="houseDescription">
+            <h3 class="descTitle">Description</h3>
+            Parfaite pour une famille, idéalement située à proximité de tout, pour habiter ou pour investir, cette maison plutôt récente construite en 1997 bien tenue, offre 128 m² utiles dont 119 habitables et, un vrai garage. Elle est aménagée sur 2 niveaux, le tout sur un beau terrain orienté est-ouest de 495 m².
+            D’abord une situation stratégique à Séné, en limite de Vannes, au cœur d’un quartier résidentiel : à moins 900 m, les premiers commerces (boulangerie et pharmacie), le parc de Tohannic avec son étang et son jardin d’enfants, l’école St Patern (maternelle et primaire) et le collège Cousteau. Puis, à moins de 2 km les centres commerciaux du Poulfanc et des Quais de Séné et ensuite, à moins de 3 km le port de Vannes, la gare, l’Hôpital Chubert et l’échangeur vers Lorient, Nantes et Rennes.
+            Ensuite, une maison bien distribuée et bien pensée : un bel espace de vie traversant et lumineux, d’est en ouest, de plus de 46 m² avec l’entrée, le séjour-salon et la cuisine, et plusieurs accès au jardin aménagé avec goût, et aux 2 terrasses. De la cuisine, qui date de 2012, aménagée et équipée (four 2012, plaques 2018, hotte 2012), il y a également une porte vers le garage de plus de 19 m². Puis toujours de plain-pied, un sas dessert le wc, la 1ère chambre, 11,7 m², et une salle d’eau. Un joli escalier vous permet d’accéder à l’étage où le palier illuminé par un beau vélux, distribue, la salle de bains, 7,6 m² utiles dont 6 m² habitables, le wc indépendant avec son vélux également, et les 3 autres chambres : 9,2 m² habitables pour 11 m² utiles et surtout, 11,8 et 13,4 m² habitables, pour 13,9 et 15,5 m² utiles.
+            Enfin, une maison bien construite, de construction traditionnelle : ardoises naturelles, charpente traditionnelle, cloisons briques et plâtre, chauffage au gaz naturel par radiateurs, la chaudière a été changée en 2020, volets roulants électriques au rdc (…).
+            Prix frais d'agence inclus 487.900 € dont frais d'agence 12900 € TTC soit 2,71 % TTC.
+            DPE : C / GES : D
+            TF : 1055 €
+            </p>
+
+            <div class="fees" title="Honoraire"><strong>Honoraires</strong> : Le prix comprend les honoraires à la charge de l'acheteur</div>
+        </div>
+
+    </div>
+
+</main>
+
+<?php include("footer.php") ?>
