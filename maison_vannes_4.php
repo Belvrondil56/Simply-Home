ff8c3aa6626782ff2adcbd6217da2450608970ef
Alexandre K <alexandre.keruzore@gmail.com>
2021-04-09 14:02:22 +0200

    ajouts images maisons vannes et ajout pages descriptions maisons vannes

---
+++ maison_vannes_4.php
@@ -0,0 +1,37 @@
+<?php include("header.php") ?>
+
+<main class="houseSingle largeurMax">
+
+    <h2 class="title">Maison 75 m² Vannes</h2>
+
+    <div class="house">
+
+        <div class="houseImg">
+            <img src="images/images/vannes_4.jpg" alt="vannes_4">
+        </div>
+
+        <div class="houseInfoBlock">
+            <div class="infos">
+                <div class="city" title="Ville"><strong><img src="images/images/localisation.png" alt="Localisation">Ville</strong> : Vannes 56000</div>
+                <div class="surface" title="Surface"><strong><img src="images/images/meters.png" alt="Surface">Surface</strong> : 75m²</div>
+                <div class="price" title="Prix"><strong><img src="images/images/price.png" alt="Prix">Prix</strong> : 333 600€</div>
+                <div class="energetic" title="Classe énergie"><strong><img src="images/images/energy.png" alt="Classe énergie">Classe énergie</strong> : A</div>
+                <div class="year" title="Année"><strong><img src="images/images/calendar.png" alt="Année">Année</strong> : 2019</div>
+            </div>
+
+            <p class="houseDescription">
+            <h3 class="descTitle">Description</h3>
+            Exclusivité. A moins de 10 minutes de la Mairie de Vannes, projet d'une Maison ossature bois de plain pied de 75 m² dans un quartier recherché. Plan personnalisable composé d'une pièce de vie de 35 m², deux chambres, un grand cellier. Chauffage poêle à granulés. Norme RT2012 avec des résultats dépassant les recommandations actuelles. Sur un terrain viabilisé de 540 m², orienté sud/ouest sans vis-à-vis. Belles prestations et environnemet de qualité.
+            Nos atouts :
+            Temps de construction réduit à 7 mois Fabrication 100% française. Isolation thermique ultra -performant Garanties constructeur/ Contrat CMI (photos et images non contractuelles)
+            Référence annonce : FY164
+            </p>
+
+            <div class="fees" title="Honoraire"><strong>Honoraires</strong> : Le prix comprend les honoraires à la charge de l'acheteur</div>
+        </div>
+
+    </div>
+
+</main>
+
+<?php include("footer.php") ?>
